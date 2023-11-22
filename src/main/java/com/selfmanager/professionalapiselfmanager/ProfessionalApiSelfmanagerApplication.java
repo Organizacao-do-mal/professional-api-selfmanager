42ce55fc512b5ee760630fe159b6e9634c80abee
elias.oliveira <elias.oliveira@adviseu.com.br>
2023-11-22 15:53:03 -0300

    initial commit

---
+++ src/main/java/com/selfmanager/professionalapiselfmanager/ProfessionalApiSelfmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.selfmanager.professionalapiselfmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalApiSelfmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalApiSelfmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfmanager/professionalapiselfmanager/ProfessionalApiSelfmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfmanager.professionalapiselfmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalApiSelfmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
